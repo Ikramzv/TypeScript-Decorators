196b6017db19b7173e7ba5460f585f91ea62665b
Ikram <ikramzulfugar@gmail.com>
2023-04-30 05:06:39 +0400

    SetId decorator added

--- index.ts
+++ index.ts
@@ -1,28 +1,34 @@
+import { randomUUID } from "crypto"
+
 function Log() {
     return (target: any, name: string, desc: PropertyDescriptor) => {
-        const origin = desc.value
+        const self = target.constructor.prototype
+        const origin = self[name] || desc.value
+        console.log(self.date) // COMPILATION TIME ; date is undefined
         desc.value = function (...args : any[]) {
+            console.log(self.date) // RUNTIME : date is the current date .
             return origin.apply(this, args)
         }
     }
 }
 
-function TimeLine() {
-    return function (constructor: any) {
+function TimeLine(date?: Date) {
+    const factory = function (constructor: any) {
         Object.defineProperty(constructor.prototype, "date" , {
             get () {
-                return new Date()
+                return date ? date : new Date() // Both are same thing, but it is just for showing that up to readers how does it work
             },
             set(v) {
                 throw new Error("Readonly property can not be updated")
             }
         })
     }
+    return factory
 }
 
 function Measure(measurer?: ((comment: string) => void), comment?: string) {
     return function (target: any, name: string , desc: PropertyDescriptor) {
-        const original = desc.value
+        const original = desc.value // Refers to original
         desc.value = function (...args: any[]) {
             const d = new Date().getTime()
             console.log(this)
@@ -39,7 +45,18 @@ function Measure(measurer?: ((comment: string) => void), comment?: string) {
     }
 }
 
-@TimeLine()
+function SetId(constructor: any) {
+    Object.defineProperty(constructor.prototype, "_id", {
+        get() {
+            return randomUUID()
+        },
+        enumerable: false,
+        configurable: false,
+    })
+}
+
+@TimeLine(new Date())
+@SetId
 class Time {
     readonly type: string
     date: Date
@@ -49,7 +66,7 @@ class Time {
     }
 
     @Log()
-    @Measure(function(){}, "")
+    // @Measure(function(){}, "")
     time() {
         for(let i = -1; i < 100000000; i++);
         return this.date.getTime()
@@ -58,4 +75,4 @@ class Time {
 
 const date = new Time("DATE")
 
-console.log(date.time())
+console.log("RETURNED : ", date.time())
