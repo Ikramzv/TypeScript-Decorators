427db314cdb36f75dc97d737b3c66cdfdfb1eb3d
Ikram <ikramzulfugar@gmail.com>
2023-04-30 05:48:12 +0400

    Comments added to clarify how does code work .

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 import { randomUUID } from "crypto"
 
 function Log() {
-    return (target: any, name: string, desc: PropertyDescriptor) => {
-        const self = target.constructor.prototype
-        const origin = self[name] || desc.value
-        console.log(self.date) // COMPILATION TIME ; date is undefined
-        desc.value = function (...args : any[]) {
-            console.log(self.date) // RUNTIME : date is the current date .
-            return origin.apply(this, args)
+    return (target: any, methodName: string, desc: PropertyDescriptor) => {
+        const self = target // or target.constructor.prototype
+
+        const origin = desc.value // Refers to value passed through previous decorator.
+        // We can access by doing self[name] but it will refer to the original function that is declared in the class
+
+        console.log("Compile Time :",self.date) // COMPILATION TIME ; date is undefined
+        desc.value = function () {
+            console.log("Runtime :",self.date) // RUNTIME : date is the current date declared by TimeLine decorator.
+            return origin.apply(this, arguments)
         }
     }
 }
@@ -27,20 +30,20 @@ function TimeLine(date?: Date) {
 }
 
 function Measure(measurer?: ((comment: string) => void), comment?: string) {
-    return function (target: any, name: string , desc: PropertyDescriptor) {
+    return function (t: any, n: string , desc: PropertyDescriptor) {
         const original = desc.value // Refers to original
-        desc.value = function (...args: any[]) {
+        desc.value = function () {
             const d = new Date().getTime()
             console.log(this)
-            const res = original.apply(this, args)
+            const res = original.apply(this, arguments)
             const xtime = new Date().getTime() - d
 
             const result = `Returned : ${res}`
-            const text = `Execution time has been ${xtime}ms\n${result}`
-            if(measurer) measurer(typeof comment !== "undefined" ? `${comment}${result}` : text)
+            const text = `Execution time has been ${xtime}ms; ${result}`
+            if(measurer) measurer(typeof comment !== "undefined" ? `${comment}; ${result}` : text)
             else console.log(text)
 
-            return original.apply(this, args)
+            return original.apply(this, arguments)
         }
     }
 }
@@ -66,9 +69,9 @@ class Time {
     }
 
     @Log()
-    // @Measure(function(){}, "")
+    @Measure(console.warn)
     time() {
-        for(let i = -1; i < 100000000; i++);
+        for(let i = -1; i < 1000000000; i++);
         return this.date.getTime()
     }
 }
